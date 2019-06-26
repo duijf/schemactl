cbf618b1c6e35de84fd2d1cabf5d7255668547ef
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-06-26 23:51:24 +0200

    cli: Add basic parser for the migration index

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Cli where
 
 import qualified Crypto.Hash as Hash
 import qualified Crypto.Hash.Algorithms as Hash
-import           Data.Text (Text)
-import qualified Data.Text as Text
+import qualified Data.Attoparsec.ByteString.Char8 as Atto
+import qualified Data.ByteString.Char8 as Bs
 
 
-data Rev = Rev Text
+data Rev = Rev Bs.ByteString
   deriving (Show)
 
 
@@ -14,11 +16,43 @@ data Migration
   = Migration
   { mRev :: Rev
   , mParent :: Maybe Rev
-  , mDescription :: Text
+  , mDescription :: Bs.ByteString
   , mUpgradeFile :: FilePath
   , mUpgradeFileHash :: Hash.Digest Hash.SHA256
   , mDowngradeFile :: Maybe FilePath
   } deriving (Show)
+
+
+data MigrationLine
+  = MigrationLine
+  { mlRev :: Rev
+  , mlParent :: Maybe Rev
+  , mlDescription :: Bs.ByteString
+  , mlBaseFilePath :: FilePath
+  }
+
+
+indexFileP :: Atto.Parser [MigrationLine]
+indexFileP = do
+  migrationLines <- Atto.many1 (migrationLineP)
+  pure migrationLines
+
+
+migrationLineP :: Atto.Parser MigrationLine
+migrationLineP = do
+  revB <- Atto.takeWhile (\c -> c /= '_')
+  Atto.char '_'
+
+  description <- Atto.takeWhile (\c -> c /= '.')
+  Atto.string ".sql"
+  Atto.char '\n'
+
+  pure MigrationLine
+    { mlRev = Rev revB
+    , mlDescription = description
+    , mlParent = Nothing -- Unknown yet
+    , mlBaseFilePath = Bs.unpack (revB <> "_" <> description <> ".sql")
+    }
 
 
 main :: IO ()
