836d9a516d08ca69a8634fa7577cc5d4a6e87a46
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-06-26 23:37:11 +0200

    cli: Model the core Migration type

--- src/Cli.hs
+++ src/Cli.hs
@@ -1,4 +1,25 @@
 module Cli where
 
+import qualified Crypto.Hash as Hash
+import qualified Crypto.Hash.Algorithms as Hash
+import           Data.Text (Text)
+import qualified Data.Text as Text
+
+
+data Rev = Rev Text
+  deriving (Show)
+
+
+data Migration
+  = Migration
+  { mRev :: Rev
+  , mParent :: Maybe Rev
+  , mDescription :: Text
+  , mUpgradeFile :: FilePath
+  , mUpgradeFileHash :: Hash.Digest Hash.SHA256
+  , mDowngradeFile :: Maybe FilePath
+  } deriving (Show)
+
+
 main :: IO ()
 main = putStrLn "Hello schemactl"
