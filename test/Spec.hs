03277db00e4e425fbe89a3494e34b911edc0be02
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-07-15 23:38:42 +0200

    test: Add hspec-discover

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,1 @@
-import qualified Cli as Cli
-import           Test.Hspec
-
-main :: IO ()
-main = hspec $ do
-  describe "noop test" $ do
-    it "runs and succeeds" $ do
-      1 `shouldBe` 1
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
